8a81449618bb11466a724b81071784b757a05919
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-06-09 23:08:30 +0530

    Implement AST and Parser Classes
    - Added LeafNode class to represent leaf nodes in the AST (e.g., integers, strings, identifiers).
    - Introduced Node class to represent nodes in the AST with left and right children.
    - Created ParseException class for handling parsing errors.
    - Developed ParseTree class to construct the AST from a list of tokens, implementing various parsing methods.
    - Implemented AST class to manage the root node and standardization of the AST.
    - Enhanced parsing logic to handle expressions, declarations, and function forms.
    - Added methods for building the AST tree structure and traversing nodes.

---
+++ src/parser /LeafNode.java
@@ -0,0 +1,22 @@
+package parser;
+
+public class LeafNode extends Node {
+    /*
+     * class to represent leaf nodes :- integers, strings, identifiers etc
+     */
+    private String value;
+
+    public LeafNode(String type, String value) {
+        super(type);
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String val) {
+        this.value = val;
+    }
+
+}
